eb272882df8493c173f2778b610ca94337e1b030
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-25 23:16:36 -0400

    feat: add Create Comment Dto

---
+++ Models/Dtos/CreateCommentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Museo.Models.Dtos
+{
+    public record CreateCommentDto
+    {
+        [Required]
+        [MinLength(10)]
+        public required string Content { get; init; }
+        [Required]
+        public Guid CanvaId { get; init; }
+    }
+}
