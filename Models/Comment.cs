20f05e1ddbc60bd92408c58f7a6bceeb144161dd
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-25 23:14:14 -0400

    feat: add Comment model

---
+++ Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using Museo.Models;
+
+namespace Museo.Models
+{
+    public class Comentario
+    {
+        public Guid Id { get; set; }
+        public required string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Guid UserId { get; set; }
+        public User User { get; set; } = default!;
+
+        public Guid CanvaId { get; set; }
+        public Canvas Canva { get; set; } = default!;
+    }
+}
+
