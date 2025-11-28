12d7de182dc526b004a72593208e65b736508323
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-27 20:19:28 -0400

    feat: Add ICommentRepositor

---
+++ Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Museo.Models;
+
+namespace Museo.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task<Comment?> GetOne(Guid id);
+
+        Task<IEnumerable<Comment>> GetByCanvasId(Guid canvasId);
+        Task Add(Comment comment);
+        Task Update(Comment comment);
+        Task Delete(Comment comment);
+    }
+}
