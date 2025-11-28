73a7965782e6285ab9bda360e595ac498704f840
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-27 20:18:30 -0400

    feat: Add Comment Response Dto

---
+++ Models/Dtos/CommentResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Museo.Models.Dtos
+{
+    public record CommentResponseDto
+    {
+        public Guid Id { get; init; }
+        public required string Content { get; init; }
+        public DateTime CreatedAt { get; init; } = DateTime.Now;
+        public Guid CanvasId { get; init; }
+        public Guid UserId { get; init; }
+        public string Username { get; init; } = string.Empty;
+    }
+}
+
