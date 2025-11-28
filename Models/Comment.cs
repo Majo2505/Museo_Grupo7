4f3702c3acab0b52321b67086fff77a369b5b8ca
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-27 20:16:17 -0400

    feat: Add Comment Model

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,17 +1,23 @@
 ﻿using Museo.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Museo.Models
 {
     public class Comment
     {
         public Guid Id { get; set; }
-        public required string Content { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public Guid UserId { get; set; }
-        public User User { get; set; } = default!;
 
-        public Guid CanvaId { get; set; }
-        public Canvas Canva { get; set; } = default!;
+        [Required, StringLength(500)] 
+        public required string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Relation N:1 with User
+        public Guid UserId { get; set; }
+        public User User { get; set; } = default!; 
+
+        // Relation N:1 wirh Canvas
+        public Guid CanvasId { get; set; } 
+        public Canvas Canvas { get; set; } = default!;
     }
 }
-
