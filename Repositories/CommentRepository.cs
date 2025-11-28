2efc4e8bf20b4987ed372354192be60aa26a1426
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-27 20:27:40 -0400

    feat: Implement Comment Repository

---
+++ Repositories/CommentRepository.cs
@@ -0,0 +1,53 @@
+﻿using Museo.Data;
+using Museo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Museo.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CommentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Comment?> GetOne(Guid id)
+        {
+            return await _context.Comments
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<Comment>> GetByCanvasId(Guid canvasId)
+        {
+            return await _context.Comments
+                .Include(c => c.User) 
+                .Where(c => c.CanvasId == canvasId)
+                .OrderByDescending(c => c.CreatedAt)
+                .ToListAsync();
+        }
+
+        
+        public async Task Add(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+        }
+
+     
+        public async Task Update(Comment comment)
+        {
+            _context.Comments.Update(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        
+        public async Task Delete(Comment comment)
+        {
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
